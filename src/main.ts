923289f273da73b85170fd4b604c5c8bf180271c
HowardMui <howardmui.2020@gmail.com>
2023-07-05 03:16:27 +0800

    feat: add swagger in main, auth.dto

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  // app.use(cookieParser());
+  app.use(cookieParser());
   app.enableCors({ origin: true, credentials: true });
+  const config = new DocumentBuilder()
+    .setTitle('Howard social media apps')
+    .setDescription('The social media API document ')
+    .setVersion('1.0')
+    .addTag('Howard social media apps')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
