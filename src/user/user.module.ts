b1236de227fdadb7a40696212d6609498adcbd6b
HowardMui <howardmui.2020@gmail.com>
2023-07-10 03:14:56 +0800

    feat: add role guard for role validation

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { RolesGuard } from 'src/auth/role-guard/roles.guard';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, RolesGuard],
 })
 export class UserModule {}
