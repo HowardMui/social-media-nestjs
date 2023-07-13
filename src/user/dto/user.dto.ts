90b888ebb5c5373cd62bcdd34d920ec2ec27a83e
HowardMui <howardmui@appicidea.com>
2023-07-13 12:33:10 +0800

    refactor: combine to one dto in user

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,4 +1,7 @@
+import { QueryParams } from 'src/types';
 import { IsOptional, IsString, Max } from 'class-validator';
+
+export class GetUserQueryParams extends QueryParams {}
 
 export class UpdateUserDTO {
   @IsString()
