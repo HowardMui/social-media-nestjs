1c7773d10aa43adad6dab5c7bf6ced7b531de117
HowardMui <howardmui.2020@gmail.com>
2023-08-13 20:31:34 +0800

    feat: add getUser dto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,34 +1,8 @@
 import { QueryParamsWithFilter, TimeStamp } from 'src/types';
-import { IsOptional, IsString, Max } from 'class-validator';
 
-export class GetUserQueryParamsWithFilter extends QueryParamsWithFilter {}
+export class GetUserListQueryParams extends QueryParamsWithFilter {}
 
-export class UpdateUserDTO {
-  @IsString()
-  @IsOptional()
-  @Max(20, { message: 'Maximum 20 chars' })
-  firstName?: string;
-
-  @IsOptional()
-  @IsString()
-  @Max(20, { message: 'Maximum 20 chars' })
-  LastName?: string;
-
-  @IsOptional()
-  @IsString()
-  image?;
-
-  @IsOptional()
-  @IsString()
-  bio?;
-
-  @IsOptional()
-  @IsString()
-  @Max(200, { message: 'Maximum 200 chars' })
-  description?;
-}
-
-export class UserProfileResponse extends TimeStamp {
+export class UserResponse extends TimeStamp {
   userId: number;
   firstName: string;
   LastName: string;
@@ -38,4 +12,14 @@ export class UserProfileResponse extends TimeStamp {
   bio: string;
   description: string;
   isVerified: boolean;
+}
+
+export class GetUserListResponse extends UserResponse {
+  followersCount: number;
+  followingCount: number;
+}
+
+export class GetOneUserResponse extends UserResponse {
+  followersCount: number;
+  followingCount: number;
 }
