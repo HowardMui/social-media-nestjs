40fc9da747559d299d52878d509c9c259388c0ed
HowardMui <howardmui@appicidea.com>
2023-09-15 19:00:42 +0800

    feat: use build in Logger with console.log

--- src/cronjobs/cronjobs.service.ts
+++ src/cronjobs/cronjobs.service.ts
@@ -1,17 +1,18 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import moment from 'moment';
 import { PrismaSrcService } from 'src/prisma-src/prisma-src.service';
 
 @Injectable()
 export class CronjobsService {
   constructor(private prisma: PrismaSrcService) {}
+  private readonly logger = new Logger(CronjobsService.name);
 
-  @Cron(CronExpression.EVERY_QUARTER)
+  @Cron(CronExpression.EVERY_QUARTER, { name: 'Reset logTable' })
   async deleteUserLogTableEverMinute() {
     try {
+      this.logger.warn("It's time to reset user table log");
       await this.prisma.logTable.deleteMany({});
-      console.log('deleted user table log', moment().toISOString());
+      this.logger.warn('Log table was reset successfully');
       return { status: HttpStatus.OK };
     } catch (err) {
       console.log(err);
