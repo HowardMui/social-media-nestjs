171013cf65bd491993144821444d796a3193cea8
HowardMui <howardmui@appicidea.com>
2023-09-19 10:54:37 +0800

    refactor: create cache module, rate limiting module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaSrcModule } from './prisma-src/prisma-src.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { MeModule } from './me/me.module';
 import { AdminModule } from './admin/admin.module';
 import { PostModule } from './post/post.module';
@@ -14,10 +14,8 @@ import { FileModule } from './file/file.module';
 import { TagModule } from './tag/tag.module';
 import { SearchModule } from './search/search.module';
 import { CronjobsModule } from './cronjobs/cronjobs.module';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
-import { CacheModule, CacheStore } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-store';
+import { CustomCacheModule } from './cache/custom-cache.module';
+import { RateLimitModule } from './rate-limit/rate-limit.module';
 
 @Module({
   imports: [
@@ -34,48 +32,10 @@ import { redisStore } from 'cache-manager-redis-store';
     TagModule,
     SearchModule,
     CronjobsModule,
-    ThrottlerModule,
-    ThrottlerModule.forRoot([
-      {
-        name: 'short',
-        ttl: 1000,
-        limit: 4,
-      },
-      {
-        name: 'medium',
-        ttl: 10000,
-        limit: 10,
-      },
-    ]),
-    CacheModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (config: ConfigService) => {
-        const store = await redisStore({
-          socket: {
-            host: config.get('REDIS_HOST'),
-            port: config.get('REDIS_PORT'),
-          },
-          database: 0,
-          username: config.get('REDIS_USERNAME'),
-          password: config.get('REDIS_PASSWORD'),
-          ttl: 60,
-        });
-
-        return {
-          store: store as unknown as CacheStore,
-        };
-      },
-      inject: [ConfigService],
-      isGlobal: true,
-    }),
+    RateLimitModule,
+    CustomCacheModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/rate-limit/rate-limit.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+
+@Module({
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 4,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 10,
+      },
+    ]),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+})
+export class RateLimitModule {}
