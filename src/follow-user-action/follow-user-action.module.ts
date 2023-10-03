bad80f210dc53893ab8ef57662a28653eb31aee4
HowardMui <howardmui@appicidea.com>
2023-10-03 19:58:37 +0800

    feat: add redis module in follow module

--- src/follow-user-action/follow-user-action.module.ts
+++ src/follow-user-action/follow-user-action.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FollowUserActionService } from './follow-user-action.service';
 import { FollowUserActionController } from './follow-user-action.controller';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
+  imports: [RedisModule],
   providers: [FollowUserActionService],
-  controllers: [FollowUserActionController]
+  controllers: [FollowUserActionController],
 })
 export class FollowUserActionModule {}
