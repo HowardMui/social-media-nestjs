c6175ac14e815de6562bf2cd50afe53789a340e8
HowardMui <howardmui.2020@gmail.com>
2023-10-11 04:10:52 +0800

    feat: add sequelize other error handling

--- src/error-handler/index.ts
+++ src/error-handler/index.ts
@@ -1,9 +1,17 @@
-import { ForbiddenException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Error } from 'sequelize';
 
 export const errorHandler = (err: Error | any) => {
   switch (err.name) {
     case 'SequelizeUniqueConstraintError':
       return new ForbiddenException(err.errors[0].message);
+    case 'SequelizeForeignKeyConstraintError':
+      return new NotFoundException(`${err.table} not exist`);
+    case 'SequelizeValidationError':
+      return new BadRequestException(err.errors[0].message);
   }
 };
