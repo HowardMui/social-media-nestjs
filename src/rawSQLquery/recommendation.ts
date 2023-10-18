69cc5d1457fa41c55793e7a4a026f7f4028fc42c
HowardMui <howardmui.2020@gmail.com>
2023-10-19 05:31:20 +0800

    feat: add isFollowing in user recommendation rawQuery

--- src/rawSQLquery/recommendation.ts
+++ src/rawSQLquery/recommendation.ts
@@ -1,15 +1,24 @@
 import { rawQueryParams } from 'src/types';
 
-export const recommendUserList = ({ limit, offset }: rawQueryParams) => {
+export const recommendUserList = ({
+  limit,
+  offset,
+  userId,
+}: rawQueryParams) => {
+  console.log('userId', userId);
   return `
-        select u.*,
-        IFNULL(up.postCount * 8, 0) + IFNULL(urp.rePostCount * 6, 0) + 
-        IFNULL(ulp.likePostCount * 4, 0) + IFNULL(ubp.bookmarkCount * 2, 0) AS score
-             from users as u
+        select 
+          u.*,
+          IFNULL(up.postCount * 8, 0) + IFNULL(urp.rePostCount * 6, 0) + 
+          IFNULL(ulp.likePostCount * 4, 0) + IFNULL(ubp.bookmarkCount * 2, 0) AS score,
+          CASE WHEN uf.followerId IS NULL THEN "FALSE" ELSE "TRUE" END AS isFollowing
+        from users as u
         left join (select userId, count(*) as 'postCount' from posts group by userId) as up on u.userId = up.userId
         left join (select userId, count(*) as 'rePostCount' from user_rePost group by userId) as urp on u.userId = urp.userId
         left join (select userId, count(*) as 'likePostCount' from user_likePost group by userId) as ulp on u.userId = ulp.userId
         left join (select userId, count(*) as 'bookmarkCount' from user_bookmarkPost group by userId) as ubp on u.userId = ubp.userId
+        LEFT JOIN userFollows AS uf ON u.userId = uf.followingId AND uf.followerId = ${userId}
+        where u.userId != ${userId}
         order by score desc
         limit ${limit ?? 20}
         offset ${offset ?? 0}
