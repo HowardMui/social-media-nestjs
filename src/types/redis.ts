ba473b2b5b601856ff60c8d13c368b65b8f7601e
HowardMui <howardmui.2020@gmail.com>
2023-10-18 17:55:41 +0800

    feat: add redis key

--- src/types/redis.ts
+++ src/types/redis.ts
@@ -4,4 +4,16 @@ export enum RedisKey {
 
   // * gmpb = get my post bookmark
   書籤 = 'gmpb',
+
+  // * gmpf = get me profile
+  我的資料 = 'gmpf',
+
+  // * gamp = get all my posts
+  我的帖文 = 'gamp',
+
+  // * grt = get recommend tags
+  推薦標籤 = 'grt',
+
+  // * gru = get recommend users
+  推薦用戶 = 'gru',
 }
